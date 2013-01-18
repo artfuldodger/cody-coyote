1554d3472930b8f8a8b49478e759833cdf135949
Jon Evans <jle@foraker.com>
2013-01-18 15:24:53 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodyCoyote::Application.config.secret_token = '8f0a53cf2795662162dea370fdb1f87d6e68488b853b62cc7a7624a264715472a1f1dc6a31215e45b06911536658beccaef99dbf6a9c05852fb0fe3a49c7fd70'
